ca35b13526dd9f807b616e1b3c93618a03363c1c
YamilethMC <L21170016@culiacan.tecnm.mx>
2025-12-20 21:17:11 -0700

    Solo la primer palabra lleva mayúscula

--- src/features/surveys/components/SurveyList/index.js
+++ src/features/surveys/components/SurveyList/index.js
@@ -15,7 +15,7 @@ const SurveyList = ({ surveys, filters, loading, meta, page, setPage, onEdit, on
             <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
             </svg>
-            Agregar Encuesta
+            Agregar encuesta
           </button>
         </div>
         <div className="space-y-4">
@@ -47,7 +47,7 @@ const SurveyList = ({ surveys, filters, loading, meta, page, setPage, onEdit, on
           <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
           </svg>
-          Agregar Encuesta
+          Agregar encuesta
         </button>
       </div>
 
