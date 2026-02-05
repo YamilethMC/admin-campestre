77b07cf924c34ed6dbd66e56ba10132473d08198
Manuela Yamileth Mendoza Cital <yamileth.mendoza@sinaloa.gob.mx>
2026-02-05 09:37:14 -0700

    Se ocultó el botón de ver lista en eventos

--- src/features/events/components/EventRegistrations/index.js
+++ src/features/events/components/EventRegistrations/index.js
@@ -346,12 +346,12 @@ const EventRegistrations = ({
           >
             Agregar socio
           </button>
-          <button
+          {/*<button
             onClick={() => alert('Funcionalidad de ver lista no implementada aún')}
             className="px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
           >
             Ver lista
-          </button>
+          </button>*/}
         </div>
       </div>
 
